3ca6b5c64068f0ab0e424fbf4a483fd2e6237c2d
Jonathan PH <jonathan_perez_hdz@hotmail.com>
2023-11-19 03:48:14 -0600

    Modified user model and use "resources" to routes

--- routes/web.php
+++ routes/web.php
@@ -10,23 +10,12 @@ use App\Http\Controllers\ConceptController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SaleController;
 use App\Http\Controllers\EntryController;
-use App\Htpp\Controllers\OutputController;
+use App\Http\Controllers\OutputController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
+// Index
 Route::get('/', function () {
     return view('index');
 });
-
 
 // Auth
 Route::get('/register', [AuthController::class, 'create'])->name('register.create');
@@ -38,44 +27,28 @@ Route::post('/login', [AuthController::class, 'authenticate'])->name('auth.authe
 Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
 
 // Users
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::resource('users', UserController::class)->except(['store', 'create']);
 
 // Roles
-Route::get('/roles', [RoleController::class, 'index'])->name('roles.index');
-Route::get('/roles/create', [RoleController::class, 'create'])->name('roles.create');
-Route::post('/roles', [RoleController::class, 'store'])->name('roles.store');
+Route::resource('roles', RoleController::class);
 
 // Suppliers
-Route::get('/suppliers', [SupplierController::class, 'index'])->name('suppliers.index');
-Route::get('/suppliers/create', [SupplierController::class, 'create'])->name('suppliers.create');
-Route::post('/suppliers', [SupplierController::class, 'store'])->name('suppliers.store');
+Route::resource('suppliers', SupplierController::class);
 
 // Customers
-Route::get('/customers', [CustomerController::class, 'index'])->name('customers.index');
-Route::get('/customers/create', [CustomerController::class, 'create'])->name('customers.create');
-Route::post('/customers', [CustomerController::class, 'store'])->name('customers.store');
+Route::resource('customers', CustomerController::class);
 
 // Concepts
-Route::get('/concepts', [ConceptController::class, 'index'])->name('concepts.index');
-Route::get('/concepts/create', [ConceptController::class, 'create'])->name('concepts.create');
-Route::post('/concepts', [ConceptController::class, 'store'])->name('concepts.store');
+Route::resource('concepts', ConceptController::class);
 
 // Products
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
-Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+Route::resource('products', ProductController::class);
 
 // Sales
-Route::get('/sales', [SaleController::class, 'index'])->name('sales.index');
-Route::get('/sales/create', [SaleController::class, 'create'])->name('sales.create');
-Route::post('/sales', [SaleController::class, 'store'])->name('sales.store');
+Route::resource('sales', SaleController::class);
 
 // Entries
-Route::get('/entries', [EntryController::class, 'index'])->name('entries.index');
-Route::get('/entries/create', [EntryController::class, 'create'])->name('entries.create');
-Route::post('/entries', [EntryController::class, 'store'])->name('entries.store');
+Route::resource('entries', EntryController::class);
 
 // Outputs
-Route::get('/outputs', [OutputController::class, 'index'])->name('outputs.index');
-// Route::get('/outputs/create', [OutputController::class, 'create'])->name('outputs.create');
-Route::post('/outputs', [OutputController::class, 'store'])->name('outputs.store');
+Route::resource('outputs', OutputController::class);
