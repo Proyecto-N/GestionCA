f095bb1049ce45d309937920746eb27608c394e7
Jonathan PH <jonathan_perez_hdz@hotmail.com>
2023-11-28 17:53:10 -0600

    Fixed create users script

--- scripts/create_users.sql
+++ scripts/create_users.sql
@@ -1,9 +1,9 @@
 USE gestionca;
 
-INSERT INTO roles ('role_id', 'role', 'description', 'created_at', 'updated_at') VALUES
+INSERT INTO roles (`role_id`, `role`, `description`, `created_at`, `updated_at`) VALUES
 (1, 'administrador', 'Tiene permisos de lectura, escritura y edición', '2023-11-11 04:52:41', '2023-11-11 04:52:41'),
 (2, 'editor', 'Tiene permisos de lectura, escritura y edición pero con algunas restricciones', '2023-11-11 04:55:59', '2023-11-11 04:55:59'),
 (3, 'lector', 'Solo tiene permisos de lectura', '2023-11-11 04:56:41', '2023-11-11 04:56:41');
 
-INSERT INTO roles ('user_id', 'name', 'surnames', 'email', 'phone', 'password', 'remember_token', 'role', 'created_at', 'updated_at') VALUES
-('e423f25d-8061-11ee-aa43-0242ac120002', 'Gestion', 'CA', 'gestionca@hotmail.com', '5512345678', '$2y$12$JcbmYjpqheQykywf1wV7C.VfzfCycyBnis10zOpMHWLJh4683GyNi', null, 1, '2023-11-11 07:14:02', '2023-11-11 07:14:02');
+INSERT INTO roles (`user_id`, `name`, `surnames`, `email`, `phone`, `password`, `remember_token`, `role`, `created_at`, `updated_at`) VALUES
+('9aa61745-e76b-4eae-89b9-d400b33d830d', 'Gestion', 'CA', 'gestionca@test.com', '5512345678', '$2y$12$VCnSHpZ1ktJ0YJHZQRQLGeJkuDgIOPOCSV6JaR1PGx2pVs0isq6gy', null, 1, '2023-11-11 07:14:02', '2023-11-11 07:14:02');
